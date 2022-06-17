40335eb4d7e66de6f3df6eaf8cce0d087e52ab3b
Chisom Jude <judegodstimechisom@gmail.com>
2022-06-17 16:16:03 +0100

    Update index.php
    updated DB details

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 
 		// PHP Data Objects(PDO) Sample Code:
 		try {
-			$conn = new PDO("sqlsrv:server = tcp:chisomjude.database.windows.net,1433; Database = covidcenter", "chisomjude", "{your_password_here}");
+			$conn = new PDO("sqlsrv:server = tcp:covidesearch.mysql.database.azure.com,1433; Database = covidsearchcenter", "chisomjude", "{your_password_here}");
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 		catch (PDOException $e) {
@@ -54,8 +54,8 @@
 		}
 
 		// SQL Server Extension Sample Code:
-		$connectionInfo = array("UID" => "chisomjude", "pwd" => "Favoured0205", "Database" => "covidcenter", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-		$serverName = "tcp:chisomjude.database.windows.net,1433";
+		$connectionInfo = array("UID" => "chisomjude", "pwd" => "Favoured0205", "Database" => "covidsearchcenter", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+		$serverName = "tcp:covidesearch.mysql.database.azure.com,1433";
 		$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
